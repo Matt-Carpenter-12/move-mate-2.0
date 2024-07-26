78145eed948dcad97011ca4eaede1fd24ae5e913
Roger_Aponte <rogeraponte10@gmail.com>
2024-07-26 18:21:57 -0400

    utils/auth

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
-// require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.Port || 3001;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// The server is started after the database is opened
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
 db.once('open', () => {
-    app.listen(PORT, function() {
-        console.log(`Listening on Port ${PORT}`);
-    });
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
