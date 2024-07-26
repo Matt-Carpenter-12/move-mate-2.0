ab50c39419b0a4aa3e62694ae1250b06a8609309
Roger_Aponte <rogeraponte10@gmail.com>
2024-07-26 18:40:06 -0400

    user routes

---
+++ server/routes/api/userroutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getSingleUser,
+  login,
+} = require('../../controllers/user-controllers');
+
+// import middleware
+const { authMiddleware } = require('../../utils/auth');
+
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authMiddleware, saveBook);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+
+module.exports = router;
