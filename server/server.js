249a4aff0ba5442430869b89acc80664f5cb43d0
Marly Valcin <marlyvalcin@gmail.com>
2024-07-25 21:17:27 -0400

    added install script, updated connection.js and commented out code in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.Port || 3001;
 
-app .use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // The server is started after the database is opened
