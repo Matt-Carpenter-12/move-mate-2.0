ab134e521e863b94ba262e90b44f32dee4614553
Marly Valcin <marlyvalcin@gmail.com>
2024-07-25 20:40:42 -0400

    updated the server to start after the database is opened

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const PORT = process.env.Port || 3001;
 app .use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
-app.listen(PORT, function() {
-    console.log(`Listening on Port ${PORT}`);
-})
+// The server is started after the database is opened
+db.once('open', () => {
+    app.listen(PORT, function() {
+        console.log(`Listening on Port ${PORT}`);
+    });
+});
