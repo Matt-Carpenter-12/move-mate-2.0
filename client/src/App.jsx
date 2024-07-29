57aa49661a43323924ea7855800e8c791cd7c61c
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-07-29 17:07:53 -0400

    npm installed bootstrap, added basic layout for login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Questionnaire from './Components/Questionnaire'
+import NavTabs from './Components/NavTabs'
+import Questionnaire from './Components/Questionnaire';
+import Login from './Components/login'
 
 function App() {
   const [count, setCount] = useState(0)
