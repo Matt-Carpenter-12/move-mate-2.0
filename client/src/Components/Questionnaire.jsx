bcdccbd0ce7a9c1f5524536ff5e032f1e38550a4
Saydeezr <saydeezr@gmail.com>
2024-07-29 13:25:30 -0400

    added the questionnaire component

---
+++ client/src/Components/Questionnaire.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const Questionnaire = ({props}) => {
+    return(
+        <Form>
+        <fieldset>
+        <Form.Group className="mb-3" controlId="workoutType">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+        </Form.Group>
+        <Form.Check
+            type="switch"
+            id="custom-switch"
+            label="Notify me reminders of my workout"
+        />
+        <Form.Group className="mb-3">
+          <Form.Label> Select workout type:</Form.Label>
+          <Form.Select id="disabledSelect">
+            <option>Select one</option>
+            <option>Running</option>
+            <option>PowerLifting</option>
+            <option>Strength-training</option>
+            <option>Yoga</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Select fitness level:</Form.Label>
+          <Form.Select id="disabledSelect">
+            <option>Select one</option>
+            <option>beginner</option>
+            <option>intermediate</option>
+            <option>expert</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label> Workout days per week:</Form.Label>
+          <Form.Select id="disabledSelect">
+            <option>Select one</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+          </Form.Select>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </fieldset>
+    </Form>
+    )
+};
+
+export default Questionnaire;
