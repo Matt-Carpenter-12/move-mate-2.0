deeda01371e907e609490d7d7eb601528100c881
Saydeezr <saydeezr@gmail.com>
2024-07-30 21:33:13 -0400

    made form dynamic according to user behavior

--- client/src/pages/Questionnaire.jsx
+++ client/src/pages/Questionnaire.jsx
@@ -1,46 +1,72 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Button, Form } from 'react-bootstrap';
 
-const Questionnaire = ({props}) => {
-    return(
-        <Form>
-        <fieldset>
-        <Form.Group className="mb-3">
-          <Form.Label> Select workout type:</Form.Label>
-          <Form.Select id="disabledSelect">
-            <option>Select one</option>
-            <option>Running</option>
-            <option>PowerLifting</option>
-            <option>Strength-training</option>
-            <option>Yoga</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Select fitness level:</Form.Label>
-          <Form.Select id="disabledSelect">
-            <option>Select one</option>
-            <option>beginner</option>
-            <option>intermediate</option>
-            <option>expert</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label> Workout days per week:</Form.Label>
-          <Form.Select id="disabledSelect">
-            <option>Select one</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-          </Form.Select>
-        </Form.Group>
-        
-        <Button type="submit">Submit</Button>
-      </fieldset>
-    </Form>
-    )
+
+const Questionnaire = () => {
+  const [showLevel, setShowLevel] = useState(false);
+  const [showConsistency, setShowConsistency] = useState(false);
+
+  const handleTypeInput = (e) => {
+      if (e.target.value !== 'Select one') {
+          setShowLevel(true);
+      } else {
+          setShowLevel(false);
+          setShowConsistency(false);
+      }
+  };
+
+  const handleLevelInput = (e) => {
+      if (e.target.value !== 'Select one') {
+          setShowConsistency(true);
+      } else {
+          setShowConsistency(false);
+      }
+  };
+
+  return (
+      <Form>
+          <fieldset>
+              <Form.Group className="mb-3">
+                  <Form.Label>Select workout type:</Form.Label>
+                  <Form.Select onChange={handleTypeInput}>
+                      <option>Select one</option>
+                      <option>Running</option>
+                      <option>PowerLifting</option>
+                      <option>Strength-training</option>
+                      <option>Yoga</option>
+                  </Form.Select>
+              </Form.Group>
+
+              {showLevel && (
+                  <Form.Group className="mb-3">
+                      <Form.Label>Select fitness level:</Form.Label>
+                      <Form.Select onChange={handleLevelInput}>
+                          <option>Select one</option>
+                          <option>beginner</option>
+                          <option>intermediate</option>
+                          <option>expert</option>
+                      </Form.Select>
+                  </Form.Group>
+              )}
+
+              {showConsistency && (
+                  <Form.Group className="mb-3">
+                      <Form.Label>Workout days per week:</Form.Label>
+                      <Form.Select>
+                          <option>Select one</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                      </Form.Select>
+                  </Form.Group>
+              )}
+
+              <Button type="submit">Submit</Button>
+          </fieldset>
+      </Form>
+  );
 };
 
 export default Questionnaire;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from 'react'; //was components but didnt work. Not sure where this is getting from?
+import { Link } from 'react-router-dom';
+import { createUser } from '../utils/API'; 
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
