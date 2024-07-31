814803bbbc738e93f3b2bd6ae5e3aad891b8cad3
Roger_Aponte <rogeraponte10@gmail.com>
2024-07-31 12:36:03 -0400

    sign up fix

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { createUser } from '../utils/API'; 
+import { createUser } from '../utils/auth'; 
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
