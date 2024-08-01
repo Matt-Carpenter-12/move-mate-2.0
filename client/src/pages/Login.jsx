9ad207ab0432493fa2b3e30cef77a382dc0d0091
Roger_Aponte <rogeraponte10@gmail.com>
2024-08-01 18:44:37 -0400

    added functioning login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,100 @@
+import { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { loginUser } from '../utils/API'; 
+import Auth from '../utils/auth';
 import  'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
-    return (
-        <div>
-            <h1 className="login-header">SIGN IN TO YOUR MOVE MATE ACCOUNT</h1>
-            <form>
-                <div className="mb-3">
-                    <label for="email input" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label for="password input" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-    </div>
+     // set initial form state
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const response = await loginUser(userFormData);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      email: '',
+      password: '',
+    });
+  };
+    return (<>
+        {/* This is needed for the validation functionality above */}
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          {/* show alert if server response is bad */}
+          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+            Something went wrong with your signup!
+          </Alert>
+           <h3>Please sign into your account</h3>
+  
+          <Form.Group className='mb-3'>
+            <Form.Label htmlFor='email'>Email</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Your email address'
+              name='email'
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          </Form.Group>
+  
+          <Form.Group className='mb-3'>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Your password'
+              name='password'
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Check
+              type="switch"
+              id="custom-switch"
+              label="Notify me reminders of my workout"
+          />
+          <Link to='/Questionnaire'>
+            <Button disabled={!(userFormData.email && userFormData.password)} type='submit' variant='success'>
+              Submit
+            </Button>
+          </Link>
+          
+        </Form>
+      </>
     )
 }
 
