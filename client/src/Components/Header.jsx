de9ac635af670d2b1fe2d860a04131653184742a
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-08-01 22:24:24 -0400

    added header component to log.jsx and myworkouts.jsx

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -36,32 +36,34 @@ const Header = () => {
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-          <Nav.Link
-            href="/Homepage"
-            className={currentLink === '/' ? 'nav-link active' : 'nav-link'}
-            onClick={() => onUpdateCurrentLink('/')}>
-            HOME
-          </Nav.Link>
-          <Nav.Link
-            href="/My-Workouts"
-            className={currentLink === '/My-Workouts' ? 'nav-link active' : 'nav-link'}
-            onClick={() => onUpdateCurrentLink('/My-Workouts')}>
-            MY WORKOUTS
-          </Nav.Link>
-          <Nav.Link
-            href="/Log"
-            className={currentLink === '/Log' ? 'nav-link active' : 'nav-link'}
-            onClick={() => onUpdateCurrentLink('Log')}>
-            WORKOUT LOG
-          </Nav.Link>
-          <Nav.Link
-            href="/"
-            className={currentLink === '/' ? 'nav-link active' : 'nav-link'}
-            onClick={() => onUpdateCurrentLink('')}>
-            LOG OUT
-          </Nav.Link>
-          </Nav>
-                
+                <Nav.Link
+                  href="/Homepage"
+                  className={currentLink === '/' ? 'nav-link active' : 'nav-link'}
+                  onClick={() => onUpdateCurrentLink('/')}>
+                  HOME
+                </Nav.Link>
+                <Nav.Link
+                  href="/My-Workouts"
+                  className={currentLink === '/My-Workouts' ? 'nav-link active' : 'nav-link'}
+                  onClick={() => onUpdateCurrentLink('/My-Workouts')}>
+                  MY WORKOUTS
+                </Nav.Link>
+                <Nav.Link
+                  href="/Log"
+                  className={currentLink === '/Log' ? 'nav-link active' : 'nav-link'}
+                  onClick={() => onUpdateCurrentLink('Log')}>
+                  WORKOUT LOG
+                </Nav.Link>
+                <Nav.Link
+                  href="/"
+                  className={currentLink === '/' ? 'nav-link active' : 'nav-link'}
+                  onClick={() => onUpdateCurrentLink('')}>
+                  LOG OUT
+                </Nav.Link>
+                </Nav>
+                <span className="navbar-text">
+                  <button className="vvd logout-btn" onClick={()=>console.log('Ready to log out')}><a href='/'><span>LOG OUT</span></a></button>
+                </span>
             </Navbar.Collapse>
           </Container>
         </Navbar>
