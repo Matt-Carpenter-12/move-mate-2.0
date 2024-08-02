29c3a8d9f1a1335072b35d91e5b008a61b80597f
matthew carpenter <matthewscarpenter12@gmail.com>
2024-08-01 20:15:18 -0400

    workout model

---
+++ server/models/Workout.js
@@ -0,0 +1,9 @@
+
+const mongoose = require("mongoose")
+
+const workoutSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: String,
+});
+
+module.exports = mongoose.model("workout", workoutSchema);
