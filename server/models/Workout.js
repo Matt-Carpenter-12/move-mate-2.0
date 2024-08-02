3792969c8ef4babe524451063e99a47602bf6bb4
matthew carpenter <matthewscarpenter12@gmail.com>
2024-08-01 20:49:49 -0400

    updated schema

--- server/models/Workout.js
+++ server/models/Workout.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose")
 
 const workoutSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    muscleGroup: String,
     description: String,
+    instuctions: String,
 });
 
 module.exports = mongoose.model("workout", workoutSchema);
