ad4bdb5ffd3c4d756c589b060b4c9820788bf5c4
Marly Valcin <marlyvalcin@gmail.com>
2024-08-05 19:11:40 -0400

    updated to include a model after the more info button is clicked

--- client/src/Components/GetWorkouts.jsx
+++ client/src/Components/GetWorkouts.jsx
@@ -1,7 +1,18 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Modal from 'react-bootstrap/Modal';
+
 
 function GetWorkouts() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     const [exercises, setExercises] = useState([]);
 
     const url = 'http://localhost:3001/workouts';
@@ -23,23 +34,50 @@ function GetWorkouts() {
 
     return (
         <>
-            {exercises.map((w, index) => {
-                return (
-                    <>
-                        <Card className="card" key={index} style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body key={index}>
-                                <Card.Title>{w.WorkOut}</Card.Title>
-                                <Card.Text>
-                                    {w.Explaination}
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+            <h1>Here are the workouts:</h1>
+            <container>
+                <Row xs={1} md={3} className="g-2">
+                    {Array.from({ length: 3 }).map((_, idx) => (
+                        <Col key={idx}>
+                            {
+                                exercises.map((w, index) => {
+                                    return (
+                                        <>
+                                            <Card className="card" key={index} style={{ width: '18rem' }}>
+                                                <Card.Img variant="top" src="holder.js/100px180" />
+                                                <Card.Body key={index}>
+                                                    <Card.Title>{w.WorkOut}</Card.Title>
+                                                    <Card.Text>
+                                                        {w.Explaination}
+                                                    </Card.Text>
+                                                    <Button onClick={handleShow} className='btn-color'>More Info</Button>
+                                                </Card.Body>
+                                            </Card>
 
-                    </>
-                )
+                                        </>
+                                    )
 
-            })}
+                                })
+                            }
+                        </Col >
+                    ))}
+                </Row>
+                <Modal show={show} onHide={handleClose}>
+                    console.log(exercises[0].workout)
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </container >
         </>
     )
 }
