d14b39d15bb683fb974f6444f1d79f24187924aa
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-08-05 17:26:05 -0400

    started layout for sidebar and homepage

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -3,7 +3,38 @@
 function Sidebar() {
     return (
         <>
-        
+            Fiter Exercises
+            {/*Show filters with option to remove them*/}
+            <ul><h5>Exercise Type</h5>
+                <li>Warm Up</li>
+                <li>Weight Training</li>
+                <li>Stretching</li>
+            </ul>
+            <ul><h5>Muscle Groups</h5>
+                <li>Biceps</li>
+                <li>Triceps</li>
+                <li>Chest</li>
+                <li>Back</li>
+                <li>Legs</li>
+                <li>Abs</li>
+                <li>Lats</li>
+                <li>Hamstrings</li>
+                <li>Calves</li>
+                <li>Quadriceps</li>
+                <li>Trapezius</li>
+                <li>Shoulders</li>
+                <li>Glutes</li>
+            </ul>
+            <ul><h5>Equipment Needed</h5>
+                <li>None</li>
+                <li>Resistance Bands</li>
+                <li>Dumbbells / Barbells</li>
+                <li>Bench</li>
+                <li>Kettlebell</li>
+                <li>Cable Machine</li>
+                <li>Pull-Up Bar</li>
+                <li>Smith Machine</li>
+            </ul>
         </>
     )
 }
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,24 @@
 import Header from '../Components/Header'
+import Sidebar from '../Components/Sidebar';
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 function Homepage() {
     return (
         <>
         <Header />
+        <section className="sidebar">
+            <Container>
+                <Row>
+                    <Col xs={12} md={2}>
+                        <Sidebar />
+                    </Col>
+                    <Col xs={12} md={10}>
+                        {/* workout cards with api info */}
+                    </Col>
+                </Row>
+            </Container>
+        </section>
         </>
     )
 }
