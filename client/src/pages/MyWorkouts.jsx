acceddb5839c538e59255a422f2f1bb3d41df360
Marly Valcin <marlyvalcin@gmail.com>
2024-08-05 18:50:00 -0400

    added the cors npm and updated MyWorkouts page to fetch a third party api

--- client/src/pages/MyWorkouts.jsx
+++ client/src/pages/MyWorkouts.jsx
@@ -2,6 +2,26 @@
 import Header from '../Components/Header'
 
 function MyWorkouts() {
+    
+    const [exercises, setExercises] = useState([]);
+
+    const url = 'http://localhost:3001/workouts';
+    const options = {
+        method: 'GET'
+    }
+
+    useEffect(() => {
+        console.log('workouts page loaded')
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data => {
+                setExercises(data);
+                // console.log(data[0]);
+            })
+            .catch(err => console.error(err.message))
+    }, [])
+    console.log(exercises);
+    
     return (
         <>
             <Header />
