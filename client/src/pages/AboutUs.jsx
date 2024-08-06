8c074384fdfb349b319245eb78631d7cf44271bf
Roger_Aponte <rogeraponte10@gmail.com>
2024-08-05 21:55:54 -0400

    removed descriptions

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -2,17 +2,17 @@ import NavTabs from '../Components/NavTabs'
 import roger from '../assets/images/aboutimages/Roger.png'
 import AboutCard from '../Components/AboutCard'
 const aboutdata = [{
-    img: roger, name: 'Saydeez', github: 'https://github.com/saydeezr', description: 'blah blah blah'
+    img: roger, name: 'Saydeez', github: 'https://github.com/saydeezr', description: ''
 }, {
-    img: roger, name: 'Bethany', github: 'https://github.com/bethanyproctor', description: 'blah blah blah'
+    img: roger, name: 'Bethany', github: 'https://github.com/bethanyproctor', description: ''
 },{
-    img: roger, name: 'Marly', github: 'https://github.com/marlyV1', description: 'blah blah blah'
+    img: roger, name: 'Marly', github: 'https://github.com/marlyV1', description: ''
 
 },{
-    img: roger, name: 'Matt', github: 'https://github.com/matt-carpenter-12', description: 'blah blah blah'
+    img: roger, name: 'Matt', github: 'https://github.com/matt-carpenter-12', description: ''
 
 },{
-    img: roger, name: 'Roger', github: 'https://github.com/rogretheekyogre/React-Portfolio', description: 'blah blah blah'
+    img: roger, name: 'Roger', github: 'https://github.com/rogretheekyogre/React-Portfolio', description: ''
 
 }]
 
