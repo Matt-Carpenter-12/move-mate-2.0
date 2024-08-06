61cc7087abb28e8f8c44f5f482c801523b99e2ae
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-08-05 22:20:33 -0400

    started the basic layout of the log page

--- client/src/pages/Log.jsx
+++ client/src/pages/Log.jsx
@@ -1,11 +1,25 @@
 //page for seeing the workout history
 import Header from '../Components/Header'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Log() {
     return (
         <>
             <Header />
-
+            <div className="log">
+                <h2 className='log-header'>MY WORKOUT LOG</h2>
+                <Container>
+                    <Row>
+                        <Col xs={12} lg={3}>
+                            <h2>SELECT A DATE TO SEE YOUR <span>WORKOUT HISTORY</span></h2>
+                            <p>the calendar will go here!</p>
+                        </Col>
+                        <Col lg={9}>
+                            <p>the workout cards will go here!</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </>
     )
 }
