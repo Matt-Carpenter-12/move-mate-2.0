d9946b6adae8c98c009efe2d770bed8c1672a2bd
Saydeezr <saydeezr@gmail.com>
2024-08-07 12:05:59 -0400

    cleaned up server side user routes/controllers code

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -4,6 +4,7 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 module.exports = {
+
   // get a single user by either their id or their email
   async getSingleUser({ user = null, params }, res) {
     const foundUser = await User.findOne({
@@ -16,6 +17,8 @@ module.exports = {
 
     res.json(foundUser);
   },
+
+
   // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
   async createUser({ body }, res) {
     const user = await User.create(body);
@@ -26,8 +29,9 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
+
+  
   // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
   async login({ body }, res) {
     const user = await User.findOne({ email: body.email });
     if (!user) {
--- server/routes/api/userroutes.js
+++ server/routes/api/userroutes.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
-const {
-  createUser,
-  getSingleUser,
-  login,
-} = require('../../controllers/user-controllers');
-
-// import middleware
+const { createUser, getSingleUser, login } = require('../../controllers/user-controllers');
 const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user
 
+//Route for Signup
+router.route('/signup').post(createUser);
+
+//Route for Login
 router.route('/login').post(login);
 
+//Route for profile (requires authentication middleware)
 router.route('/me').get(authMiddleware, getSingleUser);
+
 
 //TODO: make a route for /api/users/:id/form
 //make route that creates a new formSchema based on req.body from client and add to User model based on user Id 
