cf79896845ae258fd5776c8108ec5d3641e69b01
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-08-08 18:32:43 -0400

    made the sidebar responsive

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,176 +1,348 @@
-//side bar with all the filters for workouts
+import { useState } from 'react';
 import '../assets/css/sidebar.css'
-import Nav from 'react-bootstrap/Nav';
-import Dropdown from 'react-bootstrap/Dropdown';
-import NavItem from 'react-bootstrap/NavItem';
-import NavLink from 'react-bootstrap/NavLink';
 import { CiCirclePlus } from "react-icons/ci";
 import Input from './input';
+import { Offcanvas } from 'react-bootstrap';
 
 function Sidebar() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
         <div className="sidebar">
-            <h5>Add Filter <CiCirclePlus /></h5>
+             <button className='btn offcanvas-btn' onClick={handleShow}>Add Filter <CiCirclePlus /></button>
+             <div className="hide">
+             <h5>Add Filter <CiCirclePlus /></h5>
                 <h6 className='sidebar-title'>Intensity Level</h6>
                     <Input 
                     value=""
                     title="All"
-                    name="All-Levels"
+                    name="level"
                     />
                     <Input 
                     value="Beginner"
                     title="Beginner"
-                    name="Beginner"
+                    name="level"
                     />
                     <Input 
                     value="Intermediate"
                     title="Intermediate"
-                    name="Intermediate"
+                    name="level"
                     />
                     <Input 
                     value="Expert"
                     title="Expert"
-                    name="Expert"
+                    name="level"
                     />
                 <h6 className="sidebar-title">Exercise Type</h6>
                     <Input 
                     value=""
                     title="All"
-                    name="All-Types"
+                    name="types"
                     />
                     <Input 
                     value="Strength-Training"
                     title="Strength Training"
-                    name="Strength-Training"
+                    name="types"
                     />
                     <Input 
                     value="Warm-up"
                     title="Warm Up"
-                    name="Warm-up"
+                    name="types"
                     />
                     <Input 
                     value="Stretching"
                     title="Stretching"
-                    name="Stretching"
+                    name="types"
                     />
                 <h6 className="sidebar-title">Muscle Group</h6>
                     <Input 
                     value=""
                     title="All"
-                    name="All-Muscles"
+                    name="muscles"
                     />
                     <Input 
                     value="Biceps"
                     title="Biceps"
-                    name="Biceps"
+                    name="muscles"
                     />
                     <Input 
                     value="Triceps"
                     title="Triceps"
-                    name="Triceps"
+                    name="muscles"
                     />
                     <Input 
                     value="Chest"
                     title="Chest"
-                    name="Chest"
+                    name="muscles"
                     />
                     <Input 
                     value="Back"
                     title="Back"
-                    name="Back"
+                    name="muscles"
                     />
                     <Input 
                     value="Legs"
                     title="Legs"
-                    name="Legs"
+                    name="muscles"
                     />
                     <Input 
                     value="Abs"
                     title="Abs"
-                    name="Abs"
+                    name="muscles"
                     />
                     <Input 
                     value="Lats"
                     title="Lats"
-                    name="Lats"
+                    name="muscles"
                     />
                     <Input 
                     value="Hamstrings"
                     title="Hamstrings"
-                    name="Hamstrings"
+                    name="muscles"
                     />
                     <Input 
                     value="Calves"
                     title="Calves"
-                    name="Calves"
+                    name="muscles"
                     />
                     <Input 
                     value="Quadriceps"
                     title="Quadriceps"
-                    name="Quadriceps"
+                    name="muscles"
                     />
                     <Input 
                     value="Trapezius"
                     title="Trapezius"
-                    name="Trapezius"
+                    name="muscles"
                     />
                     <Input 
                     value="Shoulders"
                     title="Shoulders"
-                    name="Shoulders"
+                    name="muscles"
                     />
                     <Input 
                     value="Glutes"
                     title="Glutes"
-                    name="Glutes"
+                    name="muscles"
                     />
                 <h6 className="sidebar-title">Equipment</h6>
                     <Input 
                     value="none"
                     title="None"
-                    name="No-Equipment"
+                    name="equipment"
                     />
                     <Input 
                     value="Resistance-Bands"
                     title="Resistance Bands"
-                    name="Resistance-Bands"
+                    name="equipment"
                     />
                     <Input 
                     value="KettleBell"
                     title="KettleBell"
-                    name="KettleBell"
+                    name="equipment"
                     />
                     <Input 
                     value="Dumbbell"
                     title="Dumbbell"
-                    name="Dumbbell"
+                    name="equipment"
                     />
                     <Input 
                     value="Barbell"
                     title="Barbell"
-                    name="Barbell"
+                    name="equipment"
                     />
                     <Input 
                     value="Bench"
                     title="Bench"
-                    name="Bench"
+                    name="equipment"
                     />
                     <Input 
                     value="Cable-Machine"
                     title="Cable Machine"
-                    name="Cable-Machine"
+                    name="equipment"
                     />
                     <Input 
                     value="Pull-Up-Bar"
                     title="Pull-Up Bar"
-                    name="Pull-Up-Bar"
+                    name="equipment"
                     />
                     <Input 
                     value="Smith-Machine"
                     title="Smith Machine"
-                    name="Smith-Machine"
+                    name="equipment"
                     />
+                </div>
+                <Offcanvas show={show} onHide={handleClose} responsive="lg" className='offcanvas'>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Add Filter <CiCirclePlus /></Offcanvas.Title>
+            </Offcanvas.Header>
+
+            <Offcanvas.Body>
+            <h6 className='sidebar-title'>Intensity Level</h6>
+                    <Input 
+                    value=""
+                    title="All"
+                    name="level"
+                    />
+                    <Input 
+                    value="Beginner"
+                    title="Beginner"
+                    name="level"
+                    />
+                    <Input 
+                    value="Intermediate"
+                    title="Intermediate"
+                    name="level"
+                    />
+                    <Input 
+                    value="Expert"
+                    title="Expert"
+                    name="level"
+                    />
+                <h6 className="sidebar-title">Exercise Type</h6>
+                    <Input 
+                    value=""
+                    title="All"
+                    name="types"
+                    />
+                    <Input 
+                    value="Strength-Training"
+                    title="Strength Training"
+                    name="types"
+                    />
+                    <Input 
+                    value="Warm-up"
+                    title="Warm Up"
+                    name="types"
+                    />
+                    <Input 
+                    value="Stretching"
+                    title="Stretching"
+                    name="types"
+                    />
+                <h6 className="sidebar-title">Muscle Group</h6>
+                    <Input 
+                    value=""
+                    title="All"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Biceps"
+                    title="Biceps"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Triceps"
+                    title="Triceps"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Chest"
+                    title="Chest"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Back"
+                    title="Back"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Legs"
+                    title="Legs"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Abs"
+                    title="Abs"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Lats"
+                    title="Lats"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Hamstrings"
+                    title="Hamstrings"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Calves"
+                    title="Calves"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Quadriceps"
+                    title="Quadriceps"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Trapezius"
+                    title="Trapezius"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Shoulders"
+                    title="Shoulders"
+                    name="muscles"
+                    />
+                    <Input 
+                    value="Glutes"
+                    title="Glutes"
+                    name="muscles"
+                    />
+                <h6 className="sidebar-title">Equipment</h6>
+                    <Input 
+                    value="none"
+                    title="None"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="Resistance-Bands"
+                    title="Resistance Bands"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="KettleBell"
+                    title="KettleBell"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="Dumbbell"
+                    title="Dumbbell"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="Barbell"
+                    title="Barbell"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="Bench"
+                    title="Bench"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="Cable-Machine"
+                    title="Cable Machine"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="Pull-Up-Bar"
+                    title="Pull-Up Bar"
+                    name="equipment"
+                    />
+                    <Input 
+                    value="Smith-Machine"
+                    title="Smith Machine"
+                    name="equipment"
+                    />
+            </Offcanvas.Body>
+        </Offcanvas>
 
 
             {/* <Nav className="flex-column">
