254d99fa78740b732733d41be0d251885c5ce26f
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-08-09 01:33:44 -0400

    created day cards component and added them to homepage

--- client/src/Components/GetWorkouts.jsx
+++ client/src/Components/GetWorkouts.jsx
@@ -1,17 +1,14 @@
 import { Container, Row } from 'react-bootstrap';
-import "../assets/css/getWorkouts.css";
+import "../assets/css/homepage.css";
 import ExerciseCard from './ExerciseCard.jsx'
 
 function GetWorkouts() {
 
     return (
         <>
-            <h1 className='workouts-header'><span className="accent-color">SELECT A WORKOUT </span>TO GET STARTED</h1>
             <Container>
                 <Row >
                     <ExerciseCard />
-                    {/* <ExerciseCard />
-                    <ExerciseCard /> */}
                 </Row>
             </Container >
         </>
