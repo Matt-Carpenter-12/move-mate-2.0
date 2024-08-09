31e80ab8c4a1126c80f8f2ec65aef74941ba288f
Saydeezr <saydeezr@gmail.com>
2024-08-09 15:36:23 -0400

    made day cards selected when clicked instead of showing alert

--- client/src/Components/DayCard.jsx
+++ client/src/Components/DayCard.jsx
@@ -1,16 +1,25 @@
 import { Card } from "react-bootstrap";
 import '../assets/css/homepage.css'
+import React, { useState } from 'react';
 
-export default function DayCard({day, id}) {
+
+  const DayCard = ({ id, day }) => {
+    const [selectedCard, setSelectedCard] = useState(null);
+  
+    const handleCardClick = (id) => {
+      setSelectedCard(id);
+    };
+
   return (
     <div>
-        <Card onClick={() => alert("Hello from here")} id={id} className="day-card">
-            <Card.Body className="day-card-body">
-            <Card.Title className="day-card-title">{day}</Card.Title>
-            {/* <Card.Text></Card.Text> */}
-            </Card.Body>
+        <Card onClick={() => handleCardClick(id)} id={id} className={`day-card ${selectedCard === id ? 'selected' : ''}`}>
+          <Card.Body className="day-card-body">
+          <Card.Title className="day-card-title">{day}</Card.Title>
+          {/* <Card.Text></Card.Text> */}
+          </Card.Body>
         </Card>
-
     </div>
   )
-}
+};
+
+export default DayCard;
