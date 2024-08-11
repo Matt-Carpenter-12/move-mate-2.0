a8721b9d2d934411f7a7b3ce80324e4bd98025d0
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-08-11 18:27:29 -0400

    changed offcanvas to new sidebar, changed sidebar colors to orange from blue

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -16,7 +16,7 @@ function Sidebar() {
         setPopulateForm({ ...populateForm, [key]: value === "All" || value === "All" ? "" : value });
     }
 
-    const levels = ["all", "beginner", "intermediate", "expert"];
+    const levels = ["All", "Beginner", "Intermediate", "Expert"];
     const types = ["All", "Strength Training", "Warm Up", "Stretching"];
     const muscles = ["All", "Biceps", "Triceps", "Chest", "Back", "Legs", "Abs", "Lats", "Hamstrings", "Calves", "Quadriceps", "Trapezius", "Shoulders", "Glutes"];
     const equipment = ["None", "Resistance Bands", "KettleBell", "Dumbbell", "Barbell", "Bench", "Cable Machine", "Pull-Up Bar", "Smith Machine"];
@@ -119,224 +119,73 @@ function Sidebar() {
 
                 </div>
                 <Offcanvas show={show} onHide={handleClose} responsive="lg" className='offcanvas'>
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Add Filter <CiCirclePlus /></Offcanvas.Title>
-            </Offcanvas.Header>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Add Filter <CiCirclePlus /></Offcanvas.Title>
+                    </Offcanvas.Header>
 
-            <Offcanvas.Body>
-            <h6 className='sidebar-title'>Intensity Level</h6>
-                    <Input 
-                    value=""
-                    title="All"
-                    name="level"
-                    />
-                    <Input 
-                    value="Beginner"
-                    title="Beginner"
-                    name="level"
-                    />
-                    <Input 
-                    value="Intermediate"
-                    title="Intermediate"
-                    name="level"
-                    />
-                    <Input 
-                    value="Expert"
-                    title="Expert"
-                    name="level"
-                    />
-                <h6 className="sidebar-title">Exercise Type</h6>
-                    <Input 
-                    value=""
-                    title="All"
-                    name="types"
-                    />
-                    <Input 
-                    value="Strength-Training"
-                    title="Strength Training"
-                    name="types"
-                    />
-                    <Input 
-                    value="Warm-up"
-                    title="Warm Up"
-                    name="types"
-                    />
-                    <Input 
-                    value="Stretching"
-                    title="Stretching"
-                    name="types"
-                    />
-                <h6 className="sidebar-title">Muscle Group</h6>
-                    <Input 
-                    value=""
-                    title="All"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Biceps"
-                    title="Biceps"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Triceps"
-                    title="Triceps"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Chest"
-                    title="Chest"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Back"
-                    title="Back"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Legs"
-                    title="Legs"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Abs"
-                    title="Abs"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Lats"
-                    title="Lats"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Hamstrings"
-                    title="Hamstrings"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Calves"
-                    title="Calves"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Quadriceps"
-                    title="Quadriceps"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Trapezius"
-                    title="Trapezius"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Shoulders"
-                    title="Shoulders"
-                    name="muscles"
-                    />
-                    <Input 
-                    value="Glutes"
-                    title="Glutes"
-                    name="muscles"
-                    />
-                <h6 className="sidebar-title">Equipment</h6>
-                    <Input 
-                    value="none"
-                    title="None"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="Resistance-Bands"
-                    title="Resistance Bands"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="KettleBell"
-                    title="KettleBell"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="Dumbbell"
-                    title="Dumbbell"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="Barbell"
-                    title="Barbell"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="Bench"
-                    title="Bench"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="Cable-Machine"
-                    title="Cable Machine"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="Pull-Up-Bar"
-                    title="Pull-Up Bar"
-                    name="equipment"
-                    />
-                    <Input 
-                    value="Smith-Machine"
-                    title="Smith Machine"
-                    name="equipment"
-                    />
-            </Offcanvas.Body>
+                    <Offcanvas.Body>
+                    <h6 className='sidebar-title'>Intensity Level</h6>
+                            
+                            <Form>
+                                {levels.map(level => (
+                                    <div key={`default-${level}`} className="mb-3">
+                                        <Button
+                                            className="button-item"
+                                            variant={populateForm.level === (level === "all" ? "" : level) ? "primary" : "secondary"}
+                                            onClick={() => handleClick("level", level)}
+                                        >
+                                            {level}
+                                        </Button>
+                                    </div>
+                                ))}
+                            </Form>                
+
+                        <h6 className="sidebar-title">Exercise Type</h6>
+                            <Form>
+                                {types.map(type => (
+                                    <div key={`default-${type}`} className="mb-3">
+                                        <Button
+                                            className="button-item"
+                                            variant={populateForm.types === (type === "All" ? "" : type) ? "primary" : "secondary"}
+                                            onClick={() => handleClick("types", type)}
+                                        >
+                                            {type}
+                                        </Button>
+                                    </div>
+                                ))}
+                            </Form>
+                            
+                        <h6 className="sidebar-title">Muscle Group</h6>
+                            <Form>
+                                {muscles.map(muscle => (
+                                    <div key={`default-${muscle}`} className="mb-3">
+                                        <Button
+                                            className="button-item"
+                                            variant={populateForm.muscles === (muscle === "All" ? "" : muscle) ? "primary" : "secondary"}
+                                            onClick={() => handleClick("muscle", muscle)}
+                                        >
+                                            {muscle}
+                                        </Button>
+                                    </div>
+                                ))}
+                            </Form>
+
+                        <h6 className="sidebar-title">Equipment</h6>
+                            <Form>
+                                {equipment.map(equipment => (
+                                    <div key={`default-${equipment}`} className="mb-3">
+                                        <Button
+                                            className="button-item"
+                                            variant={populateForm.equipment === (equipment === "All" ? "" : equipment) ? "primary" : "secondary"}
+                                            onClick={() => handleClick("equipment", equipment)}
+                                        >
+                                            {equipment}
+                                        </Button>
+                                    </div>
+                                ))}
+                            </Form>
+                    </Offcanvas.Body>
         </Offcanvas>
-
-
-            {/* <Nav className="flex-column">
-                <Dropdown as={NavItem}>
-                    <Dropdown.Toggle as={NavLink} className='dropdown-title'>Intensity Level</Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item className='dropdown-item'>Beginner</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Intermediate</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Expert</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <Dropdown as={NavItem}>
-                    <Dropdown.Toggle as={NavLink} className='dropdown-title'>Exercise Type</Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item className='dropdown-item'>Warm Up</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Weight Training</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Stretching</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <Dropdown as={NavItem}>
-                    <Dropdown.Toggle as={NavLink} className='dropdown-title'>Muscle Groups</Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item className='dropdown-item'>Biceps</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Triceps</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Chest</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Back</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Legs</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Abs</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Lats</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Hamstrings</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Calves</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Quadriceps</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Trapezius</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Shoulders</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Glutes</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <Dropdown as={NavItem}>
-                    <Dropdown.Toggle as={NavLink} className='dropdown-title'>Equipment Needed</Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item className='dropdown-item'>None</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Resistance Band</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Dumbbell</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Kettlebell</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Barbell</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Bench</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Cable Machine</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Pull-Up Bar</Dropdown.Item>
-                        <Dropdown.Item className='dropdown-item'>Smith Machine</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </Nav> */}
         </div>
         </>
     )
